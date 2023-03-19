6ef4dd293af2050a99777e69499b0f0223324bd8
김병훈 <121845836+experthun@users.noreply.github.com>
2023-03-19 21:32:19 +0900

    Add files via upload

---
+++ Chapter3/ch06/VIPCustomer.java
@@ -0,0 +1,38 @@
+package ch06; //하위 클래스
+
+public class VIPCustomer extends Customer { //멤버변수를 쓰려면 메모리가 있어야함
+	
+	double salesRatio;
+	String agentID;
+	
+	/*public VIPCustomer() {
+		
+		
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+		
+		System.out.println("VIPCustomer() call");
+				
+	}*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		return price;
+	}
+
+	public String getAgentID() {
+		
+		return agentID;
+	}
+}
